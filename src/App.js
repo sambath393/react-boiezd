22855db387ea1921285fa96eed160d5a9132207b
sambath393 <callhi393@gmail.com>
2021-12-14 09:39:00 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [dsp, setDsp] = useState('');
+  const [itemList, setItemList] = useState([]);
+
+  const addItem = (e) => {
+    if (e.key === 'Enter') {
+      let arr = [...itemList];
+      arr.push({
+        dsp: dsp,
+      });
+      setItemList(arr);
+      setDsp('');
+    }
+  };
+
+  const editItem = (index, e) => {
+    setDsp(e.dsp);
+    let arr = [...itemList];
+    arr.splice(index, 1);
+    setItemList(arr);
+  };
+
+  const deleteItem = (index) => {
+    let arr = [...itemList];
+    arr.splice(index, 1);
+    setItemList(arr);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <input
+        value={dsp}
+        placeholder="item"
+        onChange={(e) => setDsp(e.target.value)}
+        onKeyDown={(e) => addItem(e)}
+      />
+      <ul>
+        {itemList?.map((load, index) => (
+          <li key={index}>
+            <span>{load.dsp}</span> &emsp;
+            <buttom onClick={() => editItem(index, load)}>edit</buttom>
+            &emsp;
+            <buttom onClick={() => deleteItem(index)}>delete</buttom>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
